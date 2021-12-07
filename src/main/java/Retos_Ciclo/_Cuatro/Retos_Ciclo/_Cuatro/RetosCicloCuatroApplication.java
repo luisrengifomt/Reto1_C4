3e90ba67ba960cbf894996cd748bffb66f8fed52
Luis Rengifo <luis.rengifo.mt@correo.usa.edu.co>
2021-12-07 09:48:03 -0500

    first commit

---
+++ src/main/java/Retos_Ciclo/_Cuatro/Retos_Ciclo/_Cuatro/RetosCicloCuatroApplication.java
@@ -0,0 +1,13 @@
+package Retos_Ciclo._Cuatro.Retos_Ciclo._Cuatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetosCicloCuatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetosCicloCuatroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Retos_Ciclo/_Cuatro/Retos_Ciclo/_Cuatro/RetosCicloCuatroApplicationTests.java
@@ -0,0 +1,13 @@
+package Retos_Ciclo._Cuatro.Retos_Ciclo._Cuatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetosCicloCuatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
